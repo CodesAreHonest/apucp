69e27b7e7c3261a9fe806525d8e458cb87a30222
YingHua Chai <yinghuachai0@gmail.com>
2019-04-22 10:40:36 +0800

    service worker and facebook login integrations

--- src/client/views/pages/Admin/Login/Login.js
+++ src/client/views/pages/Admin/Login/Login.js
@@ -7,12 +7,38 @@ class Login extends Component {
         super(props);
     }
 
+    static loadFbScript() {
+        const script = document.createElement("script");
+
+        script.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v3.2&appId=295582987871326";
+        script.async = true;
+        script.defer = true;
+        script.crossorigin = "anonymous";
+
+        document.body.appendChild(script);
+
+    }
+
+    componentDidMount () {
+
+        Login.loadFbScript();
+
+    }
+
     render() {
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col-md-6 text-center">
+                <div className="row text-center">
+                    <div className="col-md-6">
                         <div className="layout">
+                            <div id="fb-root" />
+                            <div
+                                className="fb-login-button"
+                                data-size="medium"
+                                data-button-type="login_with"
+                                data-auto-logout-link="false"
+                                data-use-continue-as="false"
+                            />
 
                         </div>
                     </div>
