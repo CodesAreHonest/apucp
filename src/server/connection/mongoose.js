875192b0a6c3cb5b688e14b38f229c6995137422
YingHua Chai <yinghuachai0@gmail.com>
2019-06-28 12:24:59 +0800

    mongodb create connection connects

--- src/server/connection/mongoose.js
+++ src/server/connection/mongoose.js
@@ -5,24 +5,24 @@ class MongoConnection {
 
     static connect() {
         const {dbHost, dbPort, dbName} = config;
+        let connection = '';
 
         try {
             // const connectionString = `mongodb://${dbHost}:${dbPort}/${dbName}?authSource=admin`;
-            const connectionString = `mongodb+srv://cluster0-j9hhr.gcp.mongodb.net/${dbName}?authSource=admin;`;
+            const connectionString = `mongodb+srv://yinghua:123@cluster0-j9hhr.gcp.mongodb.net/${dbName}?authSource=admin;`;
 
-            mongoose.connect(connectionString, {
-                user: 'yinghua',
-                pass: '123',
+            connection = mongoose.createConnection(connectionString, {
                 useNewUrlParser : true,
                 useFindAndModify: false,
                 useCreateIndex: true
             });
         }
         catch (err) {
-            console.log (`error connection to mongodb ${err}`)
+            console.log (`error connection to mongodb ${err}`);
+            process.exit(1);
         }
 
-        return mongoose;
+        return connection;
 
     }
 }
