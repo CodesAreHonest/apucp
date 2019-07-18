f3bf155bd84b36cc605bb49a113ea194d5912562
YingHua Chai <yinghuachai0@gmail.com>
2019-07-18 15:04:15 +0800

    fix Open Graph SEO

--- src/client/views/components/Common/Sidebar/index.js
+++ src/client/views/components/Common/Sidebar/index.js
@@ -6,6 +6,19 @@ import "./sidebar.css";
 class Sidebar extends Component {
     constructor(props) {
         super (props);
+
+        this._onChildrenClick = this._onChildrenClick.bind(this);
+    }
+
+    _onChildrenClick() {
+
+        const userDisplayInnerWidth = 766;
+
+        if (window.innerWidth <= userDisplayInnerWidth) {
+            let sidebarWrapper = document.querySelector('#wrapper');
+            sidebarWrapper.classList.remove('toggled');
+        }
+
     }
 
     render() {
@@ -15,16 +28,25 @@ class Sidebar extends Component {
                     APUCP
                 </div>
                 <div className="list-group list-group-flush">
-                    {/*<Link to="/admin/dashboard" className="list-group-item list-group-item-action bg-light">*/}
-                    {/*    Dashboard*/}
-                    {/*</Link>*/}
-                    <Link to="/admin/pending/confessions" className="list-group-item list-group-item-action bg-light">
+                    <Link
+                        to="/admin/pending/confessions"
+                        className="list-group-item list-group-item-action bg-light"
+                        onClick={this._onChildrenClick}
+                    >
                         Pending Confession
                     </Link>
-                    <Link to="/admin/approved/confessions" className="list-group-item list-group-item-action bg-light">
+                    <Link
+                        to="/admin/approved/confessions"
+                        className="list-group-item list-group-item-action bg-light"
+                        onClick={this._onChildrenClick}
+                    >
                         Approved Confession
                     </Link>
-                    <Link to="/admin/rejected/confessions" className="list-group-item list-group-item-action bg-light">
+                    <Link
+                        to="/admin/rejected/confessions"
+                        className="list-group-item list-group-item-action bg-light"
+                        onClick={this._onChildrenClick}
+                    >
                         Rejected Confession
                     </Link>
                 </div>
