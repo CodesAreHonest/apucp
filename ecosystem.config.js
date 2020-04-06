07cd8db157265d3153355a8ccdc088901a68b47f
Chai YingHua <yinghua@repromarketing.com>
2020-04-06 12:58:09 +0800

    [Pm2] - Ecosystem configuration file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   "apps": [{
-    "name" : "APUCP",
+    "name": "APUCP",
     "watch": ["src"],
-    "script" : "npm run start",
+    "script": "./src/server/index.js",
+    "autorestart": true,
+    "instances": 5,
+    "exec_mode": "cluster",
+    "cwd": "/var/www/html/apucp",
     "env": {
       "NODE_ENV": "development",
       "PORT": "8080"
